e5068173805ca60445aace33f151bd9c6dba778b
SK-BILAL <skbtrades9994@gmail.com>
2024-12-06 22:09:59 +0530

    Create Read-Only Variable in Shell.

---
+++ read-only-variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Name: SHAIKH BILAL
+#Date: 07/12 / 2024
+#AIM: THIS IS AN EXAMPLE OF READ ONLY VARIABLE IN SHELL SCRIPTING.   
+#
+###################################
+readonly name="READ ONLY"
+
+echo ${name}
+
+name="READ"
+echo ${name}
