75f42053431b6779a66b3b20e576e8a4d19da230
SK-BILAL <skbtrades9994@gmail.com>
2024-12-06 12:40:09 +0530

    System Defined Variables in Shell Scripting

---
+++ Sysvariable.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Name: SHAIKH BILAL
+#Date: / / 
+#AIM: System defined variables in shell scripting  
+#
+###################################
+
+echo ${SHELL}       # PRINT YOUR CURRENT SHELL FOR SCRIPTING
+
+echo ${HOME}        # PRINT HOME DIRECTORY OF CURRENT USER
+
+echo ${OSTYPE}      # TYPE OF OS
+
+echo ${PATH}
+
+echo ${$}           # PRINT PROCESS ID
+
+echo ${PPID}	    # PRINT PARENT PROCESS ID OF CHILD PROCESS
+
+echo ${HOSTNAME}
+
+echo $UID           # USER ID OF CURRENT USER
+
+sleep 5
+
+echo ${SECONDS}     # PRINT NO OF SECONDS REQUIRED TO EXECUTE THE SCRIPT.
