34da30b56996356fa815509d1a8129664c8a804c
SK-BILAL <skbtrades9994@gmail.com>
2024-12-09 12:20:46 +0530

    Arithmetic operation in shell scripting

---
+++ arithmetic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Name: SHAIKH BILAL
+#Date: 08/12/2024 
+#AIM:  THIS SHELL SCRIPT IS AN EXAMPLE OF ARITHMETIC OPERATION.
+#
+###################################
+
+a=10
+b=5
+
+echo "$((a+b))"            # ADDITION
+echo "$((a-b))"            # SUBSTRACTION
+echo "$((a*b))"            # MULTIPLY
+echo "$((a/b))"            # DIVIDE- QUOTIENT
+echo "$((a%b))"            # REMAINDER
+echo "$((b**3))"           # POWE
+
